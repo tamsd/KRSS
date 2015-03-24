3f721b7bd55cfdb96d37f5773b9dca886011d393
LoewSD1 <LoewSD1@gcc.edu>
2015-03-24 12:04:27 -0400

    Controller testing

--- InvoiceFinder/InvoiceFinder/Source Files/Controler.cs
+++ InvoiceFinder/InvoiceFinder/Source Files/Controler.cs
@@ -14,13 +14,35 @@ namespace InvoiceFinder
         static void Main()
         {
             //test
-            SearchQueue sQueue = new SearchQueue();
-            sQueue.addSearch("0.1.13.3.06262010.x");
-            sQueue.addSearch("0.1.69.9.01142012.x");
-            sQueue.addSearch("0.2.60.2.04162010.x");
-            sQueue.addSearch(".....");
 
-            Finder myF = new Finder(ref sQueue);
+            string trans_id, cust_id, store_id, reg_id, s_date, e_date;
+            SearchQueue sQueue = new SearchQueue();
+            Settings st = new Settings();
+
+            Console.Write("Please enter the transaction id: ");
+            trans_id = Console.ReadLine();
+            Console.Write("Please enter the customer id: ");
+            cust_id = Console.ReadLine();
+            Console.Write("Please enter the store id: ");
+            store_id = Console.ReadLine();
+            Console.Write("Please enter the region id: ");
+            reg_id = Console.ReadLine();
+            Console.Write("Please enter the starting date (in MMDDYYY format): ");
+            s_date = Console.ReadLine();
+            Console.Write("Please enter the ending date (x if it is not a range): ");
+            e_date = Console.ReadLine();
+
+            string invoice = trans_id + "." + cust_id + "." + store_id + "." + reg_id + "." + s_date + "." + e_date;
+
+            Console.Write(invoice);
+
+            //sQueue.addSearch("0.1.13.3.06262010.x");
+            //sQueue.addSearch("0.1.69.9.01142012.x");
+            //sQueue.addSearch("0.2.60.2.04162010.x");
+            //sQueue.addSearch(".....");
+
+            
+            Finder myF = new Finder(ref sQueue, ref st);
             List<Invoice> r = myF.execute();
             Results myR = new Results(ref r);
             for (int i = 0; i < myR.Size; i++ )
