f0d0f75476652e64f33093bdfe6289b232b087fc
LoewSD1 <LoewSD1@gcc.edu>
2015-04-19 15:30:28 -0400

    Exporter
    Not fully complete yet and isn't able to compile right now, but here is
    most of the code for it

---
+++ InvoiceFinder/InvoiceFinder/Source Files/Exporter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InvoiceFinder.BackEnd;
+
+using PPG.AC.NA.SS.SSUtilities; //Add reference to PPG DLL PPG.AC.NA.SS.SSUtilities.DLL
+using System.IO.Compression;    //Add reference to .NET Assempbly System.IO.Compression and System.IO.Compression.FileSystem
+
+namespace InvoiceFinder
+{
+    namespace BackEnd
+    {
+        public class Exporter
+        {
+
+            public Exporter() { }
+
+            public void export(List<Invoice> results, string output_path, int output_type)//call Settings accessor for output type
+            {
+                if (output_type == 0)//Separate PDFs
+                {
+                    PDFUtility _pdfUtility = new PDFUtility();
+
+                    List<string> pdfFileNames = new List<string>();
+                    pdfFileNames.Add("C:\\Sample1.PDF");
+                    pdfFileNames.Add("C:\\Sample2.PDF");
+
+
+                }
+                else if (output_type == 1)//Concatenated PDF
+                {
+                    PDFUtility _pdfUtility = new PDFUtility();
+
+                    List<string> pdfFileNames = new List<string>();
+
+                    for (int i = 0; i < results.Count; i++)
+                    {
+                        pdfFileNames.Add(results[i].Discovered_path);
+                    }
+
+                    _pdfUtility.ConcatenateFiles(pdfFileNames.ToArray(), output_path);
+                }
+                else//Zip folder
+                {
+                    //sourceDirectoryName : The path to the directory to be archived, specified as a relative or absolute path. 
+                    //                      A relative path is interpreted as relative to the current working directory.
+
+                    //destinationArchiveFileName: The path of the archive to be created, specified as a relative or absolute path. 
+                    //                            A relative path is interpreted as relative to the current working directory.
+
+
+                    string sourceDirectoryName = "C:\\AFolderContainingTheFilesYouWantToZIP\\";
+                    string destinationArchiveFileName = "C:\\MyZIPFile.ZIP";
+
+                    ZipFile.CreateFromDirectory(sourceDirectoryName, destinationArchiveFileName);
+                }
+            }
+
+        }
+    }
+}
+
