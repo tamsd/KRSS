40db6fc0b5f8ce85fac2b662383e45f86441009b
tamsd <stephend.tam@gmail.com>
2015-04-29 17:11:46 -0400

    update

--- InvoiceFinder/InvoiceFinder/Source Files/Search.cs
+++ InvoiceFinder/InvoiceFinder/Source Files/Search.cs
@@ -85,7 +85,7 @@ namespace InvoiceFinder
             }
 
             List<string> file_names;
-
+            //Dictionary<string, string> file_names; //key=>value >> filename=>regex pattern
             //Default constructor
             public Search() {
                 transID = null;
@@ -96,7 +96,7 @@ namespace InvoiceFinder
                 eDate = null;
                 List<string> file_names = new List<string>();
             }
-
+            
             //Single Args cosntructor
             public Search(string s) {
                 file_names = new List<string>();
@@ -107,28 +107,70 @@ namespace InvoiceFinder
                 transID = parts[2];
                 custID = parts[3];
                 sDate = parts[4];
-                eDate = parts[5];
                 dt_sDate = convertStringDate(sDate);
-                dt_eDate = convertStringDate(eDate);
-
+                if (parts[5] != "pdf" && parts.Length > 5){
+                    eDate = parts[5];
+                    dt_eDate = convertStringDate(eDate);
+                }
+                else{
+                    eDate = "";
+                    dt_eDate = new DateTime();
+                }
                 /*Based on input create all possible filenames that need to be searched for*/
                 string filename = storeID + "." + regID + "." + transID + "." + custID + "."; //does not have date or extension yet
-                if(eDate != "*"){
+                if(eDate != ""){
                     DateTime d = dt_sDate;
                     string string_date = "";
                     while(d <= dt_eDate){
+                        string pattern = makeRegex(parts);
                         string_date = convertDateTimeDate(d);
                         string temp_filename = filename + string_date; //adds date
-                        file_names.Add(temp_filename);
+                        file_names.Add(temp_filename/*, pattern*/);
                         d = d.AddDays(1);
                     }
                 }
                 else{
+                    //string pattern = makeRegex(parts);
                     filename = filename + sDate; //adds date
-                    file_names.Add(filename);
+                    file_names.Add(filename/*, pattern*/);
                 }
             }
-
+            //8000.2.12306.306381140006.03092015.pdf
+            //"^\\d{4}.\\d{1}.\\d{5}.\\d{12}.\\d{8}.pdf$" ---regular expression of file names
+            //function creates a regex expression based on parts
+            private string makeRegex(string[] parts){
+                string p = "^";
+                for(int i = 0; i < 4 && i < parts.Length; i++){
+                    if(parts[i] == "*"){
+                        switch(i){
+                            case 0: //storeid
+                                p += "\\d{4}.";
+                                break;
+                            case 1: //reg id
+                                p += "\\d{1}.";
+                                break;
+                            case 2: //trans id
+                                p += "\\d{5}.";
+                                break;
+                            case 3: //cust id
+                                p += "\\d{12}.";
+                                break;
+                            case 4: //sdate
+                                p += "\\d{8}.";
+                                break;
+                            case 5:
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    else{
+                        p += parts[i] + ".";
+                    }
+                }
+                p += "pdf";
+                return p;
+            }
             //Optional Parameter instructor, must name parameters entered
             public Search(string transaction = null, string customer = null, string store = null, string register = null, string start = null, string end = null) {
                 transID = transaction;
@@ -170,11 +212,15 @@ namespace InvoiceFinder
                 string string_date = d.ToString("MMddyyyy");
                 return string_date;
             }
-
-            public List<string> get_filenames_list(){
-                List<string> l = new List<string>(file_names);
-                return l;
+            public List<string> get_filenames_list()
+            {
+                List<string> d = new List<string>(file_names);
+                return d;
             }
+            //public Dictionary<string, string> get_filenames_list(){
+            //    Dictionary<string, string> d = new Dictionary<string, string>(file_names);
+            //    return d;
+            //}
             //ToString override
             public override string ToString() {
                 string s = "transID: " + transID + "\n";
