e78652d1c09ec6556740ad4fb7e8ced7290c3cba
tamsd <stephend.tam@gmail.com>
2015-04-29 21:33:52 -0400

    cleaned exporter/commented/still needs header

--- InvoiceFinder/InvoiceFinder/Source Files/Exporter.cs
+++ InvoiceFinder/InvoiceFinder/Source Files/Exporter.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * Author(s):
+ * Date:
+ * Description:
+*/
+using System;
 using System.IO;
 using System.IO.Compression;
 using System.Collections.Generic;
@@ -6,9 +11,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using InvoiceFinder.BackEnd;
-
-using PPG.AC.NA.SS.SSUtilities; //Add reference to PPG DLL PPG.AC.NA.SS.SSUtilities.DLL
-    //Add reference to .NET Assempbly System.IO.Compression and System.IO.Compression.FileSystem
+using PPG.AC.NA.SS.SSUtilities;
 
 namespace InvoiceFinder
 {
@@ -16,96 +19,101 @@ namespace InvoiceFinder
     {
         public class Exporter
         {
-            private string output_path;
-            private int output_type;
-            private Results results;
-            private Settings settings;
-            public Exporter() { }
+            private string output_path; //stores path to a directory where export() will send file(s) to
+            private int output_type;    //stores the type of export 0=list, 1=conc pdf, 2=zip
+            private Results results;    //intialized to the results passed to exporter
+            private Settings settings;  //used to set output_path and output_type
+            public Exporter() { }       //empty default constructor
             public Exporter(ref Settings sett, ref Results r)
             {
                 settings = sett;
                 results = r;
-            }
+            }   //2 argument constructor
 
-            public string export()//call Settings accessor for output type
-            {
-                DateTime today = DateTime.Now;
-                string export_path = "";
+            /* export() Function is called when user triggers event to export files stored in results. Return export path or "" if nothing exported*/
+            public string export(){
+                int count_exports = 0;          //used to track if any found invoices have been exported
+                DateTime today = DateTime.Now;  //used for timestamp on conc pdf and zip file name
+                string export_path = "";        //returned by function so that caller can open directory where export was saved
+
+                //update output path
                 output_path = settings.getOutputPath();
+                //update export type
                 output_type = settings.getOutputType();
-                if (output_type == 0)//Separate PDFs
-                {
-                    foreach (Invoice inv in results)
-                    {
-                        if(inv.Export){
+
+                if (output_type == 0) { //Separate PDFs List
+                    foreach (Invoice inv in results){ //iterate through results checking if invoice needs to be exported
+                        if(inv.Export == true){
+                            count_exports++;
                             string output = output_path + "\\" + inv.File_name;
-                            try
-                            {
+                            try{
                                 File.Copy(inv.Final_destination, output);
                             }
-                            catch
-                            {
+                            catch{
+                                //error copying 
                             }
-                            export_path = output;
+                            export_path = output; //set export_path to last copied file
                         }
                     }
                 }
-                else if (output_type == 1)//Concatenated PDF
-                {
-                    PDFUtility _pdfUtility = new PDFUtility();
-
-                    List<string> pdfFileNames = new List<string>();
-                    foreach(Invoice inv in results){
-                        if(inv.Export){
-                            pdfFileNames.Add(inv.Final_destination);
+                else if (output_type == 1){ //Concatenated PDF
+                    PDFUtility _pdfUtility = new PDFUtility();      //intialize pdf utility
+                    List<string> file_paths = new List<string>();   //List stores the full paths to files that need to be concatnated
+                    
+                    foreach(Invoice inv in results){ //iterate through results checking if invoice needs to be exported
+                        if(inv.Export == true){
+                            count_exports++;
+                            file_paths.Add(inv.Final_destination);
                         }
                     }
-                    try{
-                        _pdfUtility.ConcatenateFiles(pdfFileNames.ToArray(), output_path + @"\" + today.ToString("Hmmss.MMddyyyy") + ".export.pdf"); //****add dynamic name to output file in settings
+                    if(count_exports > 0){ //check if anything was exported, as to not make empty pdf
+                        export_path = output_path + @"\" + today.ToString("Hmmss.MMddyyyy") + ".export.pdf"; //timestamp export filename and generate path to export
+                        try{
+                            _pdfUtility.ConcatenateFiles(file_paths.ToArray(), export_path);
+                        }
+                        catch{
+                            //error with a corrupt or empty pdf
+                        }
                     }
-                    catch{
-                        //popup error deu to corrupt files
-                    }
-                    export_path = output_path + @"\concatenated.pdf";
                 }
-                else//Zip folder
-                {
-                    //sourceDirectoryName : The path to the directory to be archived, specified as a relative or absolute path. 
-                    //                      A relative path is interpreted as relative to the current working directory.
-
-                    //destinationArchiveFileName: The path of the archive to be created, specified as a relative or absolute path. 
-                    //                            A relative path is interpreted as relative to the current working directory.
-
-                    string temp_folder_location = settings.getOutputPath() + @"\zip"; //*******chanegd to some folder local to where the program is being run
+                else{ //zip file
+                    string temp_folder_location = settings.getOutputPath() + @"\zip"; //create path for a temp folder that will get zipped then deleted
                     try{
                         Directory.CreateDirectory(temp_folder_location);
                     }
                     catch{
+                        //error creating the temp directory
                     }
-                    foreach (Invoice inv in results)
-                    {
+                    foreach (Invoice inv in results) { //iterate through results checking if invoice needs to be exported
                         if(inv.Export){
+                            count_exports++;
                             string output = temp_folder_location + "\\" + inv.File_name;
                             try{
                                 File.Copy(inv.Final_destination, output);
                             }
                             catch{
+                                //error copying file
                             }
                         }
                     }
-                    string destinationArchiveFileName = output_path + @"\" + today.ToString("Hmmss.MMddyyyy") + ".export.zip";//****add dynamic name to output file in settings
-                    try{
-                        ZipFile.CreateFromDirectory(temp_folder_location, destinationArchiveFileName);
+                    if(count_exports > 0){ //check if anything was exported, as to not make empty pdf
+                        export_path = output_path + @"\" + today.ToString("Hmmss.MMddyyyy") + ".export.zip"; //path of zip file export with timestamp in zip filename
+                        try{
+                            ZipFile.CreateFromDirectory(temp_folder_location, export_path);
+                            try{
+                            Directory.Delete(temp_folder_location, true);
+                            }
+                            catch{
+                                //error deleting temp folder
+                            }
+                        }
+                        catch{
+                            //error zipping folder
+                        }
                     }
-                    catch{
-                    }
-                    try{
-                        Directory.Delete(temp_folder_location, true);
-                    }
-                    catch{
-                    }
-                    export_path = destinationArchiveFileName;
                 }
+
+                //return either "" or path where export was saved to
                 return export_path;
             }
         }
